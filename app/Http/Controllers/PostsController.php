90a6864f6d47bcb0dba4a7c2949b4d457a3c684a
avishkaME <ameddegoda143@gmail.com>
2019-09-12 20:38:38 +0530

    post adding function added

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function create(){
+        return view('posts.create');
+    }
+
+    public function store(){
+
+        $data = request()->validate([
+            'caption' => 'required',
+            'image' => 'required|image',
+        ]);
+
+        auth()->user()->posts()->create($data);
+
+         dd(request()->all());
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
+
+Route::get('/p/create', 'PostsController@create');
+
+Route::post('/p', 'PostsController@store');
