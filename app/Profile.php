9704c84c27ba900b828f06c29b1ef1ad0498e753
avishkaME <ameddegoda143@gmail.com>
2019-09-28 12:40:54 +0530

    default image set for new users

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,12 @@ class Profile extends Model
 {
     protected $guarded = [];
 
+    public function profileImage(){
+
+        $imagePath = ($this->image) ? $this->image : 'profile/fcH7awk2AG0qFgx5U3YzVSLzxy2VLTlNOybR9JyG.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
